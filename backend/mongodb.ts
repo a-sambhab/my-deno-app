f0c5b549f87316966b378670bdb9d72d214c4051
Sai Sambhab <84642011+a-sambhab@users.noreply.github.com>
2024-06-05 10:42:18 +0530

    Update mongodb.ts

--- backend/mongodb.ts
+++ backend/mongodb.ts
@@ -2,7 +2,7 @@ import {MongoClient, Bson} from "https://deno.land/x/mongo@v0.32.0/mod.ts";
 
 const client = new MongoClient();
 
-await client.connect("mongodb+srv://newtrial:ncQ2hXYHLBJ1ApJw@cluster0.zcw1uq9.mongodb.net/library?authMechanism=SCRAM-SHA-1")
+await client.connect("mongodb+srv://<username>:<password>@<cluster_url>/<database_name>?authMechanism=SCRAM-SHA-1")
 
 const db = client.database('library');
 
